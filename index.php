88f08703e0e0269c518e0281434bffdec19220de
santy_naren <santhoshnarendra@gmail.com>
2021-01-20 16:47:21 +0530

    test

--- index.php
+++ index.php
@@ -3,6 +3,17 @@
 //  SQL Commands, methods
 // $ - sign similar var in js
 
-$name = "narendra";
-echo "Hi ".$name;
+// HTML Page on serve
+// Form
+// Form data to be displayed in PHP
+$name = $_GET['name'];
+$emailId = $_GET['emailID'];
+$contactNumber = $_GET['contactNumber'];
+$message = $_GET['message'];
+
+echo $name,$emailId,$contactNumber,$message;
+
+// connecting be with db
+// posting data to db
+
 ?>
